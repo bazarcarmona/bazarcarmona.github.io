e2148ecd3795359cb4b9576768932d25faed4772
carmonayoselin <99924826+carmonayoselin@users.noreply.github.com>
2022-03-22 17:08:49 -0600

    Create action_page.php

--- action_page.php
+++ action_page.php
@@ -2,7 +2,8 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-</body>
+Your email address is: <?php echo $_POST["correo"]; ?>
+Tu domicilio es: <?php echo $_POST["domicilio"]; ?><br>
+<?php echo $_POST["Numero"]; ?>
+<?php echo $_POST["Producto"]; ?>
 </html>
